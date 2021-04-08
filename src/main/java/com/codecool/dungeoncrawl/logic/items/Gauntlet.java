ffb7da6074a5e4bd9a844e05ebe8dd2a53667b88
kasluk345 <kaszuba.lukasz@gmail.com>
2021-04-08 08:52:10 +0200

    add stats to item class

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Gauntlet.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Gauntlet.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic.items;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Gauntlet extends Item{
+    private int attack = 10;
     public Gauntlet(Cell cell) {
         super(cell);
     }
