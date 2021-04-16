3a7f57f5fce6aa0393b7fadbaefe420d9b3b478e
MatLeg25 <mat.legierskii@gmail.com>
2021-04-16 18:51:01 +0200

    read DB credentials from txt file added

--- src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
@@ -1,14 +1,21 @@
 package com.codecool.dungeoncrawl.dao;
 
+import com.codecool.dungeoncrawl.logic.MapLoader;
 import com.codecool.dungeoncrawl.logic.actors.Player;
 import com.codecool.dungeoncrawl.model.PlayerModel;
+import javafx.scene.image.Image;
 import org.postgresql.ds.PGSimpleDataSource;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class GameDatabaseManager {
     private PlayerDao playerDao;
+    private HashMap<String,String> DBcredentials = new HashMap<>();
 
     public void setup() throws SQLException {
         DataSource dataSource = connect();
@@ -22,9 +29,13 @@ public class GameDatabaseManager {
 
     private DataSource connect() throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        String dbName = "books";
-        String user = "bookkeeper";
-        String password = "1234";
+
+        System.out.println("Read DB credentials...");
+        readDBdata();
+
+        String dbName = DBcredentials.get("dbName");
+        String user = DBcredentials.get("user");
+        String password = DBcredentials.get("password");
 
         dataSource.setDatabaseName(dbName);
         dataSource.setUser(user);
@@ -35,5 +46,24 @@ public class GameDatabaseManager {
         System.out.println("Connection ok.");
 
         return dataSource;
+    }
+
+    private void readDBdata() {
+
+        try {
+            String shortPath = "src/main/java/com/codecool/dungeoncrawl/dao/DB-credentials.txt";
+            File file = new File(shortPath);
+            Scanner sc = new Scanner(file);
+
+            while(sc.hasNext()) {
+                String[] data = sc.nextLine().split(":");
+                //System.out.println(data[0]+"="+data[1]); //!display password in console!
+                DBcredentials.put(data[0],data[1]);
+            }
+            System.out.println("Successfully read DB data from the file");
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error - File Not Found");
+        }
     }
 }
